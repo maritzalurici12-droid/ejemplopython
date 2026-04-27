c3f2c2a3555110fd56f06450c7bae8575f14c0ce
maritza.lurici@uab.edu.bo <maritzalurici12@gmail.com>
2026-04-26 20:42:42 -0400

    Mejora del programa
    Se agregó nueva opción trabajar y un menú en bucle

--- ejemplo.py
+++ ejemplo.py
@@ -38,25 +38,60 @@ class HijoProdigo:
         self.hambre = 0
         self.arrepentimiento = 0
 
+    def mostrar_estado(self):
+        print(f"\nEstado de {self.nombre}:")
+        print(f"Dinero: {self.dinero}, Dignidad: {self.dignidad}, Hambre: {self.hambre}")
+
     def gastar_en_fiestas(self):
         self.dinero -= 20
         self.dignidad -= 10
         self.hambre += 5
-        print(f"{self.nombre} ha gastado dinero en fiestas. Dinero: {self.dinero}, Dignidad: {self.dignidad}, Hambre: {self.hambre}")
+        print(f"{self.nombre} ha gastado dinero en fiestas.")
 
     def invertir(self):
         self.dinero -= 30
         self.dignidad += 5
         self.hambre += 2
-        print(f"{self.nombre} ha invertido una parte de su dinero. Dinero: {self.dinero}, Dignidad: {self.dignidad}, Hambre: {self.hambre}")
+        print(f"{self.nombre} ha invertido dinero.")
 
     def ahorrar(self):
         self.dinero += 10
         self.dignidad += 2
         self.hambre -= 3
-        print(f"{self.nombre} ha ahorrado. Dinero: {self.dinero}, Dignidad: {self.dignidad}, Hambre: {self.hambre}")
-        
-opcion2 = input("Ingrese el número de la opción que desea elegir: ")
-if opcion2 == "1":    
-    hijo = HijoProdigo("AA")
-    hijo.gastar_en_fiestas()
+        print(f"{self.nombre} ha ahorrado dinero.")
+
+    def trabajar(self):
+        self.dinero += 20
+        self.dignidad += 10
+        self.hambre += 5
+        print(f"{self.nombre} ha trabajado duro.")
+
+# Pedir nombre
+nombre = input("Ingrese el nombre del jugador: ")
+hijo = HijoProdigo(nombre)
+
+while True:
+    print("\n¿Qué desea hacer?")
+    print("1. Gastar en fiestas")
+    print("2. Invertir")
+    print("3. Ahorrar")
+    print("4. Trabajar")
+    print("5. Salir")
+
+    opcion = input("Elige una opción: ")
+
+    if opcion == "1":
+        hijo.gastar_en_fiestas()
+    elif opcion == "2":
+        hijo.invertir()
+    elif opcion == "3":
+        hijo.ahorrar()
+    elif opcion == "4":
+        hijo.trabajar()
+    elif opcion == "5":
+        print("Juego terminado.")
+        break
+    else:
+        print("Opción no válida.")
+
+    hijo.mostrar_estado()
